33dc0c836bcd28581137d1776e12d8d372476d5d
Aarthi A <aarthi.adhikesavan@gmail.com>
2024-12-30 07:11:15 -0800

    Delete firebase.py

--- firebase.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate('firebase_credentials.json') 
-firebase_admin.initialize_app(cred)
-
-# Get Firestore client
-db = firestore.client()
